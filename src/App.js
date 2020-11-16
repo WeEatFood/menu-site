5ab62d59e86c2c5c5a6b96ba0b9d734469c2c20a
Matthew Mccarthy <matthewmc96@gmail.com>
2020-11-16 17:20:18 -0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 /** @format */
 
+import React from 'react';
+import * as ReactBootStrap from 'react-bootstrap';
+// import DankMemes from './Components/DankMemes';
+// import Features from './Components/Features';
+// import Pricing from './Components/Pricing';
+// import MoreDeets from './Components/MoreDeets';
+import NavBar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className='App'>
-      <p>home</p>
+      <Router>
+        <NavBar />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          {/* <Route path='/features' component={Features}>
+            <Features />
+          </Route>
+          <Route path='/pricing' component={Pricing}>
+            <Pricing />
+          </Route>
+          <Route path='/deets' component={MoreDeets}>
+            <MoreDeets />
+          </Route>
+          <Route path='/dankmemes' component={DankMemes}>
+            <DankMemes />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
