43206b67101d514618c1db08a4d05052baf909ad
Thang Khai <tnkhai94@gmail.com>
2020-11-24 20:00:54 -0700

    american brews

--- frontend/src/components/menu/brewery.js
+++ frontend/src/components/menu/brewery.js
@@ -8,63 +8,209 @@ function brewery() {
             <div className="sub__container--col">
 
                 <div className="sub__list">
-                    <h4 className="sub__list--header">DRAUGHT BEER</h4>
+                    <h5 className="sub__header--brew">DRAUGHT BEER </h5>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">BLUE MOON</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">BLUE MOON</h6>
                         <p className="menu-item__details--price">8</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">FIRESTONE 805</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">FIRESTONE 805</h6>
                         <p className="menu-item__details--price">9</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">NEW BELGIUN FAT TIRE</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">NEW BELGIUN FAT TIRE</h6>
                         <p className="menu-item__details--price">9</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">SIERRA NEVADA PALE ALE</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">SIERRA NEVADA PALE ALE</h6>
                         <p className="menu-item__details--price">10</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">STONE NEVERENDING HAZE</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">STONE NEVERENDING HAZE</h6>
                         <p className="menu-item__details--price">10</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">GUINNESS</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">GUINNESS</h6>
                         <p className="menu-item__details--price">10</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">PACIFICO</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">PACIFICO</h6>
                         <p className="menu-item__details--price">7</p>
                     </div>
 
-                    <div className="menu-item">
-                        <h4 className="menu-item__header">STELLA LEGERE</h4>
+                    <div className="brew-item">
+                        <h6 className="brew__header">STELLA LEGERE</h6>
                         <p className="menu-item__details--price">9</p>
                     </div>
 
                 </div>
 
                 <div className="sub__list">
-                    <h4 className="sub__list--header">AMERICAN PALE ALES</h4>
+                    <h5 className="sub__header--brew">AMERICAN PALE ALES</h5>
+                    <div className="brew-item">
+                        <h6 className="brew__header">2ROW NEMESIS, 6.3%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">DESCHUTES MIRROR POND, 5.0%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">LAGUNITAS SUPER CLUSTER, 8.0%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">LAGUNITAS SUMPIN' EASY, 5.0%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">MELVIN HUBERT MPA, 6.0%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">MOAB PALE ALE, 6.0%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">MODERN TIMES, 5.0%</h6>
+                        <p className="menu-item__details--price">9</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">ODELL DRUMROLL, 5.3%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">ODELL RUPTURE, 6.0%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">OMISSION PALE ALE, 5.8%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">OSKAR BLUES DALE'S, 6.5%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">PARK CITY AMERICAN PALE, 5.5%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">RITUAL PALE, 8.2%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">STONE RIPPER, 5.7%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">UPSLOPE CITRA, 5.8%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+
+
+
                 </div>
                 <div className="sub__list">
-                    <h4 className="sub__list--header">AMERICAN IPAS</h4>
+                    <h5 className="sub__header--brew">AMERICAN IPAS</h5>
+                    <div className="brew-item">
+                        <h6 className="brew__header">AVBC HOP OTTIN', 7.0%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">BALLAST POINT SCULPIN, 7.0%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">BIG SKY, 6.2%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">DOGFISH HEAD, 7.5%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">EPIC HAZY NEW ENGLAND, 7.0%</h6>
+                        <p className="menu-item__details--price">8</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">FOUNDERS CENTENNIAL, 7.2%</h6>
+                        <p className="menu-item__details--price">5</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">FULL SAIL, 6.0%</h6>
+                        <p className="menu-item__details--price">5</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">LAGUNITAS, 6.2%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">MELVIN, 7.5%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">ODELL IPA, 7.0%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">STONE, 6.9%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">STONE DELICIOUS, 7.7%</h6>
+                        <p className="menu-item__details--price">7</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">UINTA HOP NOSH, 7.3%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+                    <div className="brew-item">
+                        <h6 className="brew__header">UNITA GRAPEFRUIT HOP NASH, 7.3%</h6>
+                        <p className="menu-item__details--price">6</p>
+                    </div>
+
+
+
                 </div>
             </div>
 
 
-            <div className="menu-item">
-                <h4 className="menu-item__header">A DRINK SAMPLER</h4>
-                <p className="menu-item__details--price">8</p>
-            </div>
 
         </div>
     )
