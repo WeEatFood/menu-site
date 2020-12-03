ee819289d2df6c00374223e921867ba5a78642fa
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-02 22:44:14 -0600

    new commit

--- frontend/src/components/Online.js
+++ frontend/src/components/Online.js
@@ -1,27 +1,74 @@
 import React from 'react'
 import '../scss/online.scss'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import originalBurger from '../assets/burger.jpg'
 
 export default function Online () {
   return (
-    <div>
-      <div>
-        <h3>Bolder's Original Burger</h3>
-      </div>
-      <div>
-        <div>
-          <img src={ originalBurger } height={ 100 } alt="burger"/>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-          eius in placeat? Aliquam animi beatae debitis, deserunt dolorem
-          dolores ea esse nihil nobis obcaecati perferendis possimus qui tempore
-          veritatis, voluptatem.</p>
-        <button></button>
-        <div>
-          <p>25</p>
-        </div>
-      </div>
-    </div>
+<Container>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={originalBurger} />
+    <Card.Body>
+      <Card.Title>Bolder's Original Burger</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Checkout</Button>
+    </Card.Body>
+  </Card>
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={originalBurger} />
+    <Card.Body>
+      <Card.Title>Bolder's Original Burger</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Checkout</Button>
+    </Card.Body>
+  </Card>
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={originalBurger} />
+    <Card.Body>
+      <Card.Title>Bolder's Original Burger</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Checkout</Button>
+    </Card.Body>
+  </Card>
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={originalBurger} />
+    <Card.Body>
+      <Card.Title>Bolder's Original Burger</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Checkout</Button>
+    </Card.Body>
+  </Card>
+
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={originalBurger} />
+    <Card.Body>
+      <Card.Title>Bolder's Original Burger</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Checkout</Button>
+    </Card.Body>
+  </Card>
+</Container>
   )
 }
 
